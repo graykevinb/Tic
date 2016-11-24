e79b63c98436b6b2c069a0ea314898bb2d5e38bf
graykevinb <graykevinb@gmail.com>
2016-11-23 22:02:53 -0600

    changed class matrix to board, and matrix name to board, the board is now accessed by b.board.

--- tic.py
+++ tic.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
-#To play type in the coordinates for your move. 0:0 would be the top left.
-#0:2 would be the top right. 2:1 would be the bottom row in the middle.
+#!/usr/boardin/env python3
+#To play type in the coordinates for your move. 0:0 would boarde the top left.
+#0:2 would boarde the top right. 2:1 would boarde the boardottom row in the middle.
 
 def get_cor():
     """Gets the coordinates from the player"""
@@ -11,21 +11,21 @@ def get_cor():
                 return  inp
             else:
                 print ('type in a valid coordinate')
-                #because the input, inp,
+                #boardecause the input, inp,
                 #is invalid the loop will continue until the input is valid.
         except ValueError:
             print ('ValueError')
-            #Same as above. The loop will continue because of a value error.
+            #Same as aboardove. The loop will continue boardecause of a value error.
 
 
-class Rules(object):
+class Rules(oboardject):
 
-    def legal(self, board, move0, move1):
+    def legal(self, move0, move1):
         """This function will determine if the players move is on a empty square
     and return True for legal and False for illegal."""
         try:
             #checks if the coordinates are on a empty square.
-            if board[move0][move1] == 0:
+            if b.board[move0][move1] == 0:
                 return True
             else:
                 print ('Illegal move')
@@ -34,10 +34,10 @@ class Rules(object):
             print('IndexError')
             return False
 
-    def win_check(self, board, player):
+    def win_check(self, player):
         """Checks if the current player has won."""
         for x in range (0, 2):
-            if board.matrix[x][y] == player and board.matrix[x][y+1] == player and board.matrix[x][y+1] == player:
+            if b.board[x][y] == player and b.board[x][y+1] == player and b.board[x][y+1] == player:
                 return
             else:
                 return false
@@ -45,19 +45,19 @@ class Rules(object):
 class matrix():
 
     def __init__(self):
-        matrix = [[0, 0, 0],
+        b = [[0, 0, 0],
         	[0, 0, 0],
          	[0, 0, 0]]
 
-    def print_board(self):
-        """Prints the board"""
+    def print_b(self):
+        """Prints the b"""
         #This is why you need comments.
-        for row in self.board:
+        for row in self.b:
             print(row)
         return
 
 def main():
-
+    b = matrix
     player = 1
     turn = 0
     move0 = None
@@ -70,21 +70,21 @@ def main():
                 player = 2
             else:
                 player = 1
-            print_board(board)
+            b.board()
             #gets the coordinates
             print(player, 'turn.')
             move0 = get_cor()
             move1 = get_cor()
-            print_board(board)
+            b.board()
             print ('------------------------')
-            move_check.legal(board, move0, move1)
-            if move_check.legal(board, move0, move1) == True:
-                board[move0][move1] = player
+            move_check.legal(move0, move1)
+            if move_check.legal(move0, move1) == True:
+                b.board[move0][move1] = player
                 turn += 1
             else:
                 continue
-            if move_check.win_check(board, player):
-                print_board(board)
+            if move_check.win_check(player):
+                b.board()
                 print ('Congratulations! You won player: ')
                 print(player)
                 break
